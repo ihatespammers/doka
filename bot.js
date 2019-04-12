7ea83afc882a9fee915b90fab431902148781743
ihatespammers <45184270+ihatespammers@users.noreply.github.com>
2019-04-12 23:24:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,36 @@
-const Discord = require("discord.js");
-  const kboosh = new Discord.Client();
-    const kbooshtoken = "NTY1ODk4OTY0MjcyOTM5MDE4.XK9I0Q.Wc15NN3emMX2hhTGijPHKyD3jts"
-      kboosh.on('ready', () => {
-        kboosh.user.setGame(`xAKRAM | !b `,'https://www.twitch.tv/ROMA-SHOP');
-          console.log('Im Ready!');
-  
-        });
+const Discord = require("discord.js"); 
+const client = new Discord.Client();
+var prefix = "$";
+client.on("message", message => {
 
-  kboosh.on('message', message => {
-    if (message.content.split(' ')[0] == '!b')
-       message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-
-
-           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
-                                                      message.delete();
-            
-                                                    });
-            
-                                                  });
-   kboosh.on("message", message => {
-       var prefix = "!";
- 
-             var args = message.content.substring(prefix.length).split(" ");
-                if (message.content.startsWith(prefix + "b")) {
-                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-
-                          if (!args[1]) {
-                            
-                                 let embed3 = new Discord.RichEmbed()
-                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
-                                       .setColor("#FF00FF")
-                                          message.channel.sendEmbed(embed3);
-                            
-                                        } else {
-
-                            
-                                           let embed4 = new Discord.RichEmbed()
-                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
-                                                                .setColor("#99999")
-                               
-                                                                message.channel.sendEmbed(embed4);
-                                                      message.delete();
-                            }
-                          }
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
 });
-kboosh.login(kbooshtoken);
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as $!{client.user.tag}`);
+client.user.setGame(`Script By xAKRAM`,"http://twitch.tv/xAKRAM")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("NTY0NTAyMTM5MzA1NTkwODI1.XKoz0Q.3D0YyfIwELB1i6F2YNCwpYdz9NY");
+
+
+
+
